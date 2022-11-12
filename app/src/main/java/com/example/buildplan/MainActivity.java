470b3657bb59c07942ed6cc33c70c93b78572d7c
shivanshchhabra <https://github.com/shan18u/Android.git>
2022-11-11 21:59:59 -0800

    Initial commit with background, splash screen

---
+++ app/src/main/java/com/example/buildplan/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.buildplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout =findViewById( R.id.mainLayout);
+            //Background
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(3500);
+        animationDrawable.start();
+    }
+
+
+}
