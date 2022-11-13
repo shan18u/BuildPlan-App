82e988756f12d019f5d777f38fc0f7e55471fa7f
shivanshchhabra <https://github.com/shan18u/Android.git>
2022-11-12 18:51:09 -0800

    Nav bar and fragments

--- app/src/main/java/com/example/buildplan/MainActivity.java
+++ app/src/main/java/com/example/buildplan/MainActivity.java
@@ -1,24 +1,65 @@
 package com.example.buildplan;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
+
+import com.example.buildplan.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ConstraintLayout constraintLayout =findViewById( R.id.mainLayout);
-            //Background
+// login move
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2500);
         animationDrawable.setExitFadeDuration(3500);
         animationDrawable.start();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.back:
+                    replaceFragments(new BackFragment());
+
+                    break;
+                case R.id.home:
+                    replaceFragments(new HomeFragment());
+                    break;
+                case R.id.logout:
+                    replaceFragments(new LogOutFragment());
+                    break;
+            }
+            return true;
+
+        });
+
+    }
+
+    private void replaceFragments(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
+
+
     }
 
 
