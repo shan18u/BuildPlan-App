72aaeb047cdf3e7f2ec018b706eb014775d8414b
shan18u <shivanshchhabra18@gmail.com>
2022-12-02 20:23:29 -0800

    Log In.

--- app/src/main/java/com/example/buildplan/LogIn.java
+++ app/src/main/java/com/example/buildplan/LogIn.java
@@ -1,41 +1,126 @@
 package com.example.buildplan;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 
 public class LogIn extends AppCompatActivity {
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView google_btn;
+
+
+
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        google_btn= findViewById(R.id.google_btn);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.container);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(500);
+        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.start();
+
+
+
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
-
+        Button button11 = (Button) findViewById(R.id.button11);
         Button loginbutton = (Button) findViewById(R.id.login);
 
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
                     Toast.makeText(LogIn.this, "Success", Toast.LENGTH_SHORT).show();
                 }
 
                     else{
-                    Toast.makeText(LogIn.this, "Nope", Toast.LENGTH_SHORT).show();
+                   Toast.makeText(LogIn.this, "Nope", Toast.LENGTH_SHORT).show();
+
+
+
 
                 }
             }
         });
 
+
+        google_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+
+
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogIn.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+    void SignIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try{
+                task.getResult(ApiException.class);
+                navigateToMainActivity();
+            } catch (ApiException e){
+                Toast.makeText(getApplicationContext(), "Wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void  navigateToMainActivity(){
+        finish();
+        Intent intent = new Intent(LogIn.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
 }
