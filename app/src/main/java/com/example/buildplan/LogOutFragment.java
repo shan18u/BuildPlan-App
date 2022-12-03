a2b1b13779fdea59db6e4b27cdce8dd5358e0035
shan18u <shivanshchhabra18@gmail.com>
2022-12-02 20:24:10 -0800

    Log Out Fragment.

--- app/src/main/java/com/example/buildplan/LogOutFragment.java
+++ app/src/main/java/com/example/buildplan/LogOutFragment.java
@@ -9,12 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+
+
 public class LogOutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
-        return inflater.inflate(R.layout.fragment_log_out, container, false);
+        return inflater.inflate(R.layout.activity_log_out, container, false);
+
+
     }
+
+
+
 }
